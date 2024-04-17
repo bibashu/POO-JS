5a6b59639aa8f926f932aa8aaa71f619c168c88c
Aby Ndiaye <bibachu8@gmail.com>
2024-04-17 12:43:38 +0000

    Ajout-EXO

---
+++ POOO-EXOS/01-exo1.js
@@ -0,0 +1,34 @@
+class Persones {
+
+    constructor(nom, age){
+        this.nom = nom
+        this.age = age 
+    }
+    afficherInfoPersonnes(){
+        console.log(`Nom : ${this.nom}`)
+        console.log(`Age : ${this.age}`)
+        console.log(' ')
+    }
+}
+
+let Personne1 = new Persones('Aby', 24)
+let Personne2 = new Persones('Toto', 44)
+
+Personne1.afficherInfoPersonnes()
+Personne2.afficherInfoPersonnes()
+// exo2 ---Héritage
+class Etudiants extends Persones {
+    constructor(nom,age, Specialisation){
+        // super(): appele le constructeur de la classe parente et lui passe des arguments (nom,age)
+        super(nom, age)
+        this.Specialisation = Specialisation
+    }
+    afficherInfoPersonnes(){
+        console.log(` ${super.afficherInfoPersonnes()}`)
+        // console.log(`Nom : ${super.age}`)
+        console.log(`Spécilaisation : ${this.Specialisation}`)
+    }
+}
+let Etudiant1 = new Etudiants('Aby',24,'Develloppemen web')
+
+Etudiant1.afficherInfoPersonnes()
